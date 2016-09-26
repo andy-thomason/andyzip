a706fc8ff12d2c320e40063037c611afd3925309
Andy Thomson <andy@andythomason.com>
2016-09-26 18:50:42 +0100

    Testing suffix array

--- examples/deflate.cpp
+++ examples/deflate.cpp
@@ -10,7 +10,8 @@
 int main() {
   minizip::deflate_encoder enc;
 
-  static const uint8_t text[] = "to be or not to be, that is the question! 1234 1234 1234";
+  //static const uint8_t text[] = "to be or not to be, that is the question! 1234 1234 1234";
+  static const uint8_t text[] = "123123123";
 
   std::vector<uint8_t> buffer(0x10000);
   enc.encode(buffer.data(), buffer.data() + buffer.size(), text, text + sizeof(text) - 1);
--- include/minizip/deflate_encoder.hpp
+++ include/minizip/deflate_encoder.hpp
@@ -19,23 +19,19 @@ namespace minizip {
       addr_type group;
       addr_type next_group;
       addr_type addr;
+      addr_type depth;
     };
 
     void assign(const uint8_t *src, const uint8_t *src_max) {
       size_t size = src_max - src;
 
       sorter.resize(size + 1);
-      for (size_t i = 0; i != size; ++i) {
+      for (size_t i = 0; i != size+1; ++i) {
         sorter_t &s = sorter[i];
-        s.group = src[i];
-        s.next_group = 1;
+        s.group = i == size ? 0 : src[i];
+        s.next_group = i == size ? 0 : 1;
         s.addr = (addr_type)i;
-      }
-      {
-        sorter_t &s = sorter[size];
-        s.group = 0;
-        s.next_group = 0;
-        s.addr = (addr_type)size;
+        s.depth = 0;
       }
 
       auto sort_by_address = [](const sorter_t &a, const sorter_t &b) { return a.addr < b.addr; };
@@ -51,8 +47,15 @@ namespace minizip {
       auto debug_dump = [this, src](const char *msg, size_t h) {
         printf("\n%s h=%d\n", msg, (int)h);
         for (size_t i = 0; i != sorter.size(); ++i) {
+        //for (size_t i = 0; i != 4; ++i) {
           auto &s = sorter[i];
-          printf("%4d %12d %4d %4d %s\n", (int)i, (int)s.group, (int)s.next_group, (int)s.addr, (char*)src + s.addr);
+          size_t acp = 0;
+          if (i != 0) {
+            auto p = src + s.addr;
+            auto q = src + sorter[i-1].addr;
+            while (*p && *p == *q) { ++p; ++q; ++acp; }
+          }
+          printf("%4d %12d %4d %4d %4d %4d %s\n", (int)i, (int)s.group, (int)s.next_group, (int)s.addr, (int)s.depth, (int)acp, (char*)src + s.addr);
         }
       };
 
@@ -61,22 +64,22 @@ namespace minizip {
         // sort by group    
         std::sort(sorter.begin(), sorter.end(), sort_by_group_and_next);
     
-        if (debug) debug_dump("sorted group and next_group", h);
-
-        // build the group numbers
-        auto t = sorter[0];
-        addr_type group = 0;
-        sorter[0].group = group++;
         int finished = 1;
-        for (size_t i = 1; i != size+1; ++i) {
-          auto &s = sorter[i];
-          int different = (t.group != s.group) | (t.next_group != s.next_group);
-          finished &= different;
-          group = different ? (addr_type)i : group;
-          t = s;
-          s.group = group;
+        for (size_t i = 0; i != size+1;) {
+          auto si = sorter[i];
+          size_t j = i+1;
+          sorter[i].group = (addr_type)i;
+          int done = 0;
+          for (; j != size+1 && sorter[j].group == si.group && sorter[j].next_group == si.next_group; ++j) {
+            sorter[j].group = (addr_type)i;
+            sorter[j].depth++;
+            done = 1;
+          }
+          sorter[i].depth += done;
+          finished &= !done;
+          i = j;
         }
-                
+
         if (debug) debug_dump("built group numbers", h);
 
         if (finished) break;
@@ -90,8 +93,11 @@ namespace minizip {
           s.next_group = i < size - h ? sorter[i+h].group : 0;
         }
     
-        if (debug) debug_dump("set next_group", h);
+        //if (debug) debug_dump("set next_group", h);
       }
+
+      //std::sort(sorter.begin(), sorter.end(), sort_by_address);
+      debug_dump("final", 1);
     }
 
     sorter_t &operator[](size_t i) { return sorter[i]; }
